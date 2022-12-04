d69ea8041c3fda73aa45260a0a68c18ec4849a4d
Magnus <magnus.harryson@hotmail.com>
2022-12-04 22:53:36 +0000

    needs some work

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,21 +1,23 @@
 pub fn elf_iterator(input: &str) -> u32 {
     let ls = input.lines();
-    let mut winners: Vec<u32> = vec![0, 0, 0];
+    let mut winners = vec![0, 0, 0];
 
     let mut elf_cal = 0;
     for l in ls {
+        winners.reverse_sort();
         match l.parse::<u32>() {
             Ok(c) => elf_cal += c,
             Err(_) => {
-                if elf_cal > winner.1 {
-                    winner = elf_cal
-                }
+                let i = winners.partition_point(|&x| x < elf_cal);
+                dbg!(i);
+                winners.insert(i-1, elf_cal);
+                winners.truncate(3);
                 elf_cal = 0;
             }
         }
     }
 
-    winner
+    winners.iter().sum()
 }
 
 fn main() {
