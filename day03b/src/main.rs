87810c9190f9fa56edd333870bb5d76e4606b1be
magnus.harryson <magnus.harryson@satcube.com>
2022-12-05 16:45:09 +0100

    recent changes

--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 fn map_letter_to_int(ch: char) -> u32 {
     let alphabet = String::from_utf8((b'a'..=b'z').chain(b'A'..=b'Z').collect()).unwrap();
     match alphabet.find(ch) {
@@ -9,21 +10,19 @@ fn map_letter_to_int(ch: char) -> u32 {
 pub fn rucksack_solver(input: &str) -> u32 {
     let ls = input.lines();
     let mut score = 0;
-    let mut concatenated_slice: String = "".to_string();
+    let mut rucksacks: Vec<&str> = vec![];
     for (i, l) in ls.enumerate() {
         if (i + 1) % 3 == 0 {
-            concatenated_slice.push_str(l);
-            'inner: for ch in l.chars() {
-                if concatenated_slice.matches(ch).count() > 2 {
-                    dbg!(ch);
-                    score += map_letter_to_int(ch);
-                    break 'inner;
-                }
-            }
-            dbg!(concatenated_slice);
-            concatenated_slice = "".to_string();
+            let set_a: HashSet<_> = rucksacks[0].chars().collect();
+            let set_b: HashSet<_> = rucksacks[1].chars().collect();
+            let set_c: HashSet<_> = rucksacks[2].chars().collect();
+            let set_ab = set_a
+                .intersection(&set_b)
+                .collect::<HashSet<_>>()
+                .intersection(&set_c);
+            let ch = rucksacks.clear();
         } else {
-            concatenated_slice.push_str(l);
+            rucksacks.push(l);
         }
     }
     score
